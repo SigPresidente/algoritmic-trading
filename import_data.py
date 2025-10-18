20a037532d601a33be20a463136e6ce1a3214314
SigPresidente <andrea.poletti.94@gmail.com>
2025-10-18 11:28:16 +0200

    D: fixed naming and added account info

--- import_data.py
+++ import_data.py
@@ -7,6 +7,6 @@ from alpha_vantage.timeseries import TimeSeries
 api_key = ALPHA_API_KEY
 ts = TimeSeries(key=api_key, output_format='pandas')
 data, meta = ts.get_daily(symbol='^NDX', outputsize='full')  # NASDAQ-100
-#print(data.head())  # OHLCV data
+print(data.head())  # OHLCV data
 
 print(f"API KEY DI ALPHA: {api_key}")
